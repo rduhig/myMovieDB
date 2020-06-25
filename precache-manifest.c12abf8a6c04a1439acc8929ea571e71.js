98e034f9a2d605c1dbb1c28b793fed48590913f6
Robert Duhig <robertduhig@zoho.com>
2020-06-25 00:00:17 -0400

    Updates

---
+++ precache-manifest.c12abf8a6c04a1439acc8929ea571e71.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d26c14a2e95594208e89d778567bd043",
+    "url": "/myMovieDB/index.html"
+  },
+  {
+    "revision": "91debaad6f3469fc85e7",
+    "url": "/myMovieDB/static/css/main.1a814806.chunk.css"
+  },
+  {
+    "revision": "3b250eb67e7b4ea2c517",
+    "url": "/myMovieDB/static/js/2.b2f2bc19.chunk.js"
+  },
+  {
+    "revision": "ad951958ed7766763ed3e6c72917393c",
+    "url": "/myMovieDB/static/js/2.b2f2bc19.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "91debaad6f3469fc85e7",
+    "url": "/myMovieDB/static/js/main.48e3a4c7.chunk.js"
+  },
+  {
+    "revision": "9010b1ea7f1165a8c1aa",
+    "url": "/myMovieDB/static/js/runtime-main.2ba7ddb0.js"
+  },
+  {
+    "revision": "90d2c60e3c4c5a41ec1cec18da47d7d9",
+    "url": "/myMovieDB/static/media/cinema-vector.90d2c60e.png"
+  }
+]);
